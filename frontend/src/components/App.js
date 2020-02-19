e178251bdd38f256a107f2d43166088264bfa6d9
Ameer Hamza <juttameerhamza@gmail.com>
2020-02-20 01:20:13 +0500

    banner feature section add

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import FeatureProduct from './FeatureProduct';
 import Feature from './Feature';
 import './App.css';
 // import './vendor/jquery/jquery-3.2.1.min.js';
@@ -10,6 +11,7 @@ class App extends React.Component {
     return (
       <React.Fragment>
         <Header />
+        <FeatureProduct />
         <Feature />
         <Footer />
       </React.Fragment>
--- frontend/src/components/Feature/index.js
+++ frontend/src/components/Feature/index.js
@@ -1,45 +1,84 @@
 import React, { Component } from 'react';
 
-class Footer extends Component {
-    state = {
-        featureProducts: [
-            { name: 'banners', img: '/images/sb003.jpg' },
-            { name: 'posters', img: '/images/BACKMOUNTPOSTER.jpg' },
-            { name: 'label stickers', img: '/images/labelsticker.jpg' },
-            { name: 'arcylic photo', img: '/images/acrylicboard.jpg' },
-            { name: 'double-sided posters', img: '/images/D-SIDEDPOSTER.jpg' },
-            { name: 'canvas wall sticker', img: '/images/vinylwall.jpg' },
-            { name: 'vehicle wrap sticker', img: '/images/VehicleSticker.jpg' },
-            { name: 'vinyl lettering', img: '/images/LETTERINGsticker.jpg' },
-            { name: 'woodenFrame poster', img: '/images/WOODENPOSTER.jpg' }
-        ]
-    }
-
+class Feature extends Component {
     render() {
         return (
             <section className="newproduct bgwhite p-t-45 p-b-105">
                 <div className="container">
                     <div className="sec-title p-b-60">
-                        <h3 className="m-text5 t-center">
-                            Featured Products
-				    </h3>
+                        <h3 className="m-text5 t-center heading">
+                            ABOUT BANNERSHOP
+				        </h3>
                     </div>
                     <div className="row">
-                        {this.state.featureProducts.map((product) => {
-                            return (
-                                <div className="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
-                                    <div className="block1 hov-img-zoom pos-relative m-b-30" style={{ backgroundColor: '#000' }}>
-                                        <img src={product.img} alt="IMG-BENNER" style={{ opacity: '0.8' }} />
+                        <div className="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">               
+                            <div style={{ textAlign: 'center', width: '90%', marginBottom: '50px'}}>
+                                <i className="fa fa-globe" style={{ fontSize: '60px', color: '#e65540', marginBottom: '15px' }}></i>
+                                <h5 style={{ marginBottom: '15px', color: '#e65540', fontWeight: '600' }}>
+                                    INTERNATIONAL EXPOSURE,<br />
+                                    LOCAL KNOWLEDGE
+                                </h5>
+                                <span>
+                                As industry leaders for 10+ years, our international exposure allows us to learn from the best and bring back best practices to our customers.
+                                </span>
+                            </div>
+                        </div>
+                        <div className="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">               
+                            <div style={{ textAlign: 'center', width: '90%', marginBottom: '50px'}}>
+                                <i className="fa fa-thumbs-up" style={{ fontSize: '60px', color: '#e65540', marginBottom: '15px' }}></i>
+                                <h5 style={{ marginBottom: '15px', color: '#e65540', fontWeight: '600' }}>
+                                    SOLUTIONS DRIVEN
+                                </h5>
+                                <span>
+                                    Whether its online or offline. If you’re looking for quick, ad-hoc purchase or if you want advice from our team regarding special requirements, customized solutions, colour matching or delivery challenges.
+                                </span>
+                            </div>
+                        </div>
+                        <div className="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">               
+                            <div style={{ textAlign: 'center', width: '90%', marginBottom: '50px'}}>
+                                <i className="fa fa-volume-control-phone" style={{ fontSize: '60px', color: '#e65540', marginBottom: '15px' }}></i>
+                                <h5 style={{ marginBottom: '15px', color: '#e65540', fontWeight: '600' }}>
+                                    CUSTOMER CENTRIC
+                                </h5>
+                                <span>
+                                    A consultative approach. We listen &amp; think about our customer’s needs to offer the best marketing solution, whether the objective is budget durability, quality or campaign focused we can tailor make a solution
+                                </span>
+                            </div>
+                        </div>
 
-                                        <div className="block1-wrapbtn" style={{ width: '80%' }}>
-                                            <a href="#" className="flex-c-m m-text2 bg3 hov1 trans-0-4" style={{ padding: '8px' }}>
-                                                {product.name}
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            )
-                        })}
+                        <div className="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">               
+                            <div style={{ textAlign: 'center', width: '90%', marginBottom: '50px'}}>
+                                <i className="fa fa-ship" style={{ fontSize: '60px', color: '#e65540', marginBottom: '15px' }}></i>
+                                <h5 style={{ marginBottom: '15px', color: '#e65540', fontWeight: '600' }}>
+                                    LOGISTICS &amp; DELIVERY FOCUS
+                                </h5>
+                                <span>
+                                    National and even international delivery is available. Time is critical when comes to campaign launches, we can project manage the time frame from start to dispatch to ensure your campaign or promotion launch date is not compromised.
+                                </span>
+                            </div>
+                        </div>
+                        <div className="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">               
+                            <div style={{ textAlign: 'center', width: '90%', marginBottom: '50px'}}>
+                                <i className="fa fa-dollar" style={{ fontSize: '60px', color: '#e65540', marginBottom: '15px' }}></i>
+                                <h5 style={{ marginBottom: '15px', color: '#e65540', fontWeight: '600' }}>
+                                    VALUE FOR MONEY
+                                </h5>
+                                <span>
+                                Our website provides instant pricing and built in discount for volume jobs. Depending on your budget we can turnaround your job in as little as 24 hours. If you have more time to spare we can also offer more economical options.
+                                </span>
+                            </div>
+                        </div>
+                        <div className="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">               
+                            <div style={{ textAlign: 'center', width: '90%', marginBottom: '50px'}}>
+                                <i className="fa fa-check-square" style={{ fontSize: '60px', color: '#e65540', marginBottom: '15px' }}></i>
+                                <h5 style={{ marginBottom: '15px', color: '#e65540', fontWeight: '600' }}>
+                                    QUALITY ASSURANCE
+                                </h5>
+                                <span>
+                                We know that quality is important and offer the best fit for your requirements, quality hardware and quality prints with service to match. Bannershop offer flexibility with multiple print &amp; hardware options to suit any budget.
+                                </span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
@@ -47,7 +86,7 @@ class Footer extends Component {
     }
 }
 
-export default Footer;
+export default Feature;
 
 
 
