72870d7865225eadfa5446d5b9409718a42a1cd8
malikshahzad228 <malikshahzad228@gmail.com>
2020-02-25 16:39:41 +0500

    Customer, products, custom quotes added.

--- backend/bannershop/api/serializers/products.py
+++ backend/bannershop/api/serializers/products.py
@@ -1,9 +1,21 @@
 from rest_framework.serializers import ModelSerializer
 
-from api.models import Product
+from api.models import Product, Option, SubOption
 
 
-class ProductsSerializer(ModelSerializer):
+class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
+        fields = '__all__'
+
+
+class OptionSerializer(ModelSerializer):
+    class Meta:
+        model = Option
+        fields = '__all__'
+
+
+class SubOptionSerializer(ModelSerializer):
+    class Meta:
+        model = SubOption
         fields = '__all__'
---
+++ backend/bannershop/api/views/customers.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from rest_framework.generics import ListAPIView
+
+from api.models import Customer
+from api.serializers.customers import CustomerSerializer
+
+
+class CustomerListViewSet(ListAPIView):
+    serializer_class = CustomerSerializer
+    queryset = Customer.objects
