ab71188e8ed4b0a6d64963ebee1a9027ef688d08
hanifsarwary <hanifsarwari@addo.ai>
2020-03-04 16:35:50 +0500

    unnecessary permissions removed from APIs

--- backend/bannershop/bannershop/settings.py
+++ backend/bannershop/bannershop/settings.py
@@ -94,9 +94,6 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 20,
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
