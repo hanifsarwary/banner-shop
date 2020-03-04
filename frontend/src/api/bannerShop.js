15a3f419785bda496e76aba5a347557c2a2deddd
Ameer Hamza <juttameerhamza@gmail.com>
2020-03-04 17:59:23 +0500

    api integrated

---
+++ frontend/src/api/bannerShop.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+const cors = 'https://cors-anywhere.herokuapp.com/'
+const token = localStorage.getItem('token');
+
+axios.interceptors.response.use(null, error => {
+  console.log(error);
+});
+
+export default axios.create({
+  baseURL: cors + 'http://34.68.49.20:8001',
+  headers: {
+    Authorization: `Token ${token}`
+  }
+});
--- frontend/src/components/Category/CategoryProducts.js
+++ frontend/src/components/Category/CategoryProducts.js
@@ -1,58 +1,73 @@
 import React from 'react';
+import Loader from 'react-loader-spinner';
 import { Link } from 'react-router-dom';
 
 const CategoryProducts = (props) => {
-    return (
-        <div className="col-sm-6 col-md-8 col-lg-9 p-b-50">
-            <div className="flex-sb-m flex-w p-b-35">
-                <span className="s-text8 p-t-5 p-b-5">
-                    Showing 1–1 of {props.items.length} results
-				</span>
-            </div>
-            <div className="row">
+    if (props.loaded) {
+        return (
+            <div className="col-sm-6 col-md-8 col-lg-9 p-b-50">
+                <div className="flex-sb-m flex-w p-b-35">
+                    <span className="s-text8 p-t-5 p-b-5">
+                        Showing 1–1 of {props.items.length} results
+                    </span>
+                </div>
+                <div className="row">
+                    {props.items.map((item) => {
+                        return (
+                            <div className="col-sm-12 col-md-6 col-lg-4 p-b-50" key={item.id}>
+                                <div className="block2">
+                                    <div className="block2-img wrap-pic-w of-hidden pos-relative block2-labelnew" style={{ height: '300px' }}>
+                                        <img src={item.default_product_image} alt="IMG-PRODUCT" />
 
-                {props.items.map((item) => {
-                    return (
-                        <div className="col-sm-12 col-md-6 col-lg-4 p-b-50" key={item.id}>
-                            <div className="block2">
-                                <div className="block2-img wrap-pic-w of-hidden pos-relative block2-labelnew" style={{ height: '300px' }}>
-                                    <img src={item.imgURL} alt="IMG-PRODUCT" />
-
-                                    <div className="block2-overlay trans-0-4">
-                                        <div className="block2-btn-addcart w-size1 trans-0-4">
-                                            <button className="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
-                                                Add to Cart
-                                            </button>
+                                        <div className="block2-overlay trans-0-4">
+                                            <div className="block2-btn-addcart w-size1 trans-0-4">
+                                                <button className="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
+                                                    Add to Cart
+                                                </button>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div className="block2-txt p-t-20">
-                                    <Link to={`/product/${item.id}`} className="block2-name dis-block s-text3 p-b-5" style={{ textTransform: 'capitalize' }}>
-                                        {item.name}
-                                    </Link>
-                                    <ul className="product-feature">
-                                        {item.features.map((feature) => {
-                                            return (
-                                                <li>
-                                                    <i className="fa fa-check"></i>
-                                                    <span>{feature}</span>
-                                                </li>
-                                            )
-                                        })}
+                                    <div className="block2-txt p-t-20">
+                                        <Link to={`/product/${item.id}`} className="block2-name dis-block s-text3 p-b-5" style={{ textTransform: 'capitalize' }}>
+                                            {item.product_name}
+                                        </Link>
+                                        <ul className="product-feature">
+                                            {/* 'Size: 33.5" X 80"', 'Included Carrying Bags', '6-Month Hardware Warranty', 'Lightweight & Portable' */}
+                                            <li >
+                                                <i className="fa fa-check"></i>
+                                                <span>Included Carrying Bags</span>
+                                            </li>
+                                            <li >
+                                                <i className="fa fa-check"></i>
+                                                <span>6-Month Hardware Warranty</span>
+                                            </li>
+                                            <li >
+                                                <i className="fa fa-check"></i>
+                                                <span>Lightweight &amp; Portable</span>
+                                            </li>
 
-                                    </ul>
+                                        </ul>
 
-                                    <span className="block2-price m-text6 p-r-5 mt-1" style={{ fontWeight: 'bold', color: '#e65540' }}>
-                                        {`$${item.price}`}
-                                    </span>
+                                        <span className="block2-price m-text6 p-r-5 mt-1" style={{ fontWeight: 'bold', color: '#e65540' }}>
+                                            {`$${item.one_unit_weight}`}
+                                        </span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    );
-                })}
+                        );
+                    })}
+                </div>
             </div>
-        </div>
-    )
+        );
+    } else {
+        return (
+            <div className="col-sm-6 col-md-8 col-lg-9 p-b-50">
+                <div className="loader-container" style={{ display: 'flex', alignContent: 'center', justifyContent: 'center' }}>
+                    <Loader type="TailSpin" color="#e65540" height={80} width={80} />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default CategoryProducts;
