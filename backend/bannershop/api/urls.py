14b5bf31b00ee7c2b71fb33bc0e92015f3271f7e
hanifsarwary <hanifsarwari@addo.ai>
2020-03-05 23:20:50 +0500

    new fields and urls added

--- backend/bannershop/api/urls.py
+++ backend/bannershop/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from api.views.categories import CategoryListViewSet, SubCategoryListViewSet
 from api.views.custom_quotes import CustomQuoteViewSet
 from api.views.customers import CustomerListViewSet
-from api.views.products import ProductsListViewSet, ProductOptionsListViewSet, SubProductOptionsListViewSet, CategoryProductsViewSet
+from api.views.products import ProductsListViewSet, ProductOptionsListViewSet, SubProductOptionsListViewSet, CategoryProductsViewSet, ProductDetailViewSet
 from api.views.users import UsersListCreateViewSet, UsersDetailUpdateViewSet
 from api.views.contact_requests import ContactRequestViewSet
 from api.views.orders import OrderViewSet, ProductOrderViewSet
@@ -16,7 +16,8 @@ urlpatterns = [
     path('orders/', OrderViewSet.as_view()),
     path('product-orders/', ProductOrderViewSet.as_view()),
     path('products/', ProductsListViewSet.as_view()),
-    path('products/<int:category_id>/', CategoryProductsViewSet.as_view()),
+    path('products/<int:pk>/', ProductDetailViewSet.as_view()),
+    path('products/category/<int:category_id>/', CategoryProductsViewSet.as_view()),
     path('products/<int:product_id>/options/', ProductOptionsListViewSet.as_view()),
     path('products/<int:product_id>/options/<int:option_id>/sub-options/', SubProductOptionsListViewSet.as_view()),
     path('users/', UsersListCreateViewSet.as_view()),
