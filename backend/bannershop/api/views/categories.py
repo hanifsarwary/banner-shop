b215223c167c307ef13bf88cce437cf8a0cc7744
hanifsarwary <hanifsarwari@addo.ai>
2020-03-07 14:38:09 +0500

    New create end points added for custom requests and detail url for categories added

--- backend/bannershop/api/views/categories.py
+++ backend/bannershop/api/views/categories.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView, ListAPIView, RetrieveUpdateAPIView
 from rest_framework.response import Response
 
 from api.models import Category
 from api.serializers.categories import CategorySerializer
 
 
-class CategoryListViewSet(ListAPIView):
+class CategoryListViewSet(ListCreateAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.filter(parent_category__isnull=True).order_by('id')
 
@@ -21,3 +21,9 @@ class SubCategoryListViewSet(ListAPIView):
         queryset = self.get_queryset().filter(parent_category_id=category_id)
         serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data)
+
+
+class CategoryDetailViewSet(RetrieveUpdateAPIView):
+
+    serializer_class = CategorySerializer
+    queryset = Category.objects
--- backend/bannershop/api/views/contact_requests.py
+++ backend/bannershop/api/views/contact_requests.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
 
 from api.models import ContactRequest
 from api.serializers.contact_requests import ContactRequestSerializer
@@ -8,3 +8,10 @@ class ContactRequestViewSet(ListCreateAPIView):
 
     serializer_class = ContactRequestSerializer
     queryset = ContactRequest.objects.all().order_by('-id')
+
+
+class ContactRequestDetailViewSet(RetrieveUpdateAPIView):
+
+    serializer_class = ContactRequestSerializer
+    queryset = ContactRequest.objects
+    
