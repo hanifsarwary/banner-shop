adab561e759b49c4c9c5dfb99ef20beb0a4c8473
hanifsarwary <hanifsarwari@addo.ai>
2020-03-23 13:37:21 +0500

    pricing model in try/except block

--- backend/bannershop/api/views/pricing.py
+++ backend/bannershop/api/views/pricing.py
@@ -6,44 +6,47 @@ from api.constants import *
 class CalculatePriceViewSet(APIView):
 
     def post(self, request, *args, **kwargs):
-        product_id = request.data.get('product_id', 1)
-        product_name = request.data.get('product_name')
-        product = Product.objects.filter(pk=product_id).first()
-        option_queryset = Option.objects.filter(product=product)
-        quantity = 0
-        total_price = 0
-        if product.price_type == PRODUCT_PER_SQFT:
-            quantity = request.data['options'].pop('Quantity')
-            total_price = product.price_details['price'] * quantity * request.data.get('options').get('Width', 1) * request.data.get('options').get('Height')
-            total_price = product.price_details.get('setup_cost', 0)
-        elif product.price_type == PRODUCT_VARIABLE_PER_QUANTITY:
-            quantity = request.data['options'].pop('Quantity')
-            for k in product.price_details:
-                if quantity >= int(k.split('-')[0]) and quantity <= int(k.split('-')[1]):
-                    total_price = quantity * product.price_details.get(k)
-                    break
+        try:
+            product_id = request.data.get('product_id', 1)
+            product_name = request.data.get('product_name')
+            product = Product.objects.filter(pk=product_id).first()
+            option_queryset = Option.objects.filter(product=product)
+            quantity = 0
+            total_price = 0
+            if product.price_type == PRODUCT_PER_SQFT:
+                quantity = request.data['options'].pop('Quantity')
+                total_price = product.price_details['price'] * quantity * request.data.get('options').get('Width', 1) * request.data.get('options').get('Height')
+                total_price = product.price_details.get('setup_cost', 0)
+            elif product.price_type == PRODUCT_VARIABLE_PER_QUANTITY:
+                quantity = request.data['options'].pop('Quantity')
+                for k in product.price_details:
+                    if quantity >= int(k.split('-')[0]) and quantity <= int(k.split('-')[1]):
+                        total_price = quantity * product.price_details.get(k)
+                        break
 
-        percentage_temp_arr = []
-        for oq in option_queryset:
-            if oq.option_type == OPTION_PERCENTAGE and not oq.is_deleted:
-                if oq.is_suboptions:
-                    if request.data.get('options').get(oq.option_name):
-                        percentage_temp_arr.append(request.data.get('options').get(oq.option_name, 1)[1])
-                else:
-                    percentage_temp_arr.append(request.data.get('options').get(oq.option_name))
-            elif oq.option_type == OPTION_FLAT_RATE and not oq.is_deleted:
-                if oq.is_suboptions:
-                    if request.data.get('options').get(oq.option_name):
-                        total_price = total_price + request.data.get('options').get(oq.option_name)[1]
-                else:
-                    total_price = total_price + request.data.get('options').get(oq.option_name, 0)
+            percentage_temp_arr = []
+            for oq in option_queryset:
+                if oq.option_type == OPTION_PERCENTAGE and not oq.is_deleted:
+                    if oq.is_suboptions:
+                        if request.data.get('options').get(oq.option_name):
+                            percentage_temp_arr.append(request.data.get('options').get(oq.option_name, 1)[1])
+                    else:
+                        percentage_temp_arr.append(request.data.get('options').get(oq.option_name))
+                elif oq.option_type == OPTION_FLAT_RATE and not oq.is_deleted:
+                    if oq.is_suboptions:
+                        if request.data.get('options').get(oq.option_name):
+                            total_price = total_price + request.data.get('options').get(oq.option_name)[1]
+                    else:
+                        total_price = total_price + request.data.get('options').get(oq.option_name, 0)
             
-            else:
-                if oq.is_suboptions:
-                    if request.data.get('options').get(oq.option_name):
-                        total_price = total_price + quantity * request.data.get('options').get(oq.option_name)[1]
                 else:
-                    total_price = total_price + quantity * request.data.get('options').get(oq.option_name, 0)
-        for i in percentage_temp_arr:
-            total_price = total_price + total_price * (i / 100)     
-        return Response({"price": total_price})
+                    if oq.is_suboptions:
+                        if request.data.get('options').get(oq.option_name):
+                            total_price = total_price + quantity * request.data.get('options').get(oq.option_name)[1]
+                    else:
+                        total_price = total_price + quantity * request.data.get('options').get(oq.option_name, 0)
+            for i in percentage_temp_arr:
+                total_price = total_price + total_price * (i / 100)     
+            return Response({"price": total_price})
+        except:
+            return Response({"price": 0})
