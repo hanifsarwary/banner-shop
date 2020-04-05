52eeb254d196b3ef3a93c9040b821bdaf1bcf196
hanifsarwary <l154368@lhr.nu.edu.pk>
2020-04-05 14:38:04 +0500

    Logs added for debugging

--- backend/bannershop/api/views/pricing.py
+++ backend/bannershop/api/views/pricing.py
@@ -71,6 +71,7 @@ class CalculatePriceViewSet(APIView):
 
                 elif oq.option_type == OPTION_BASIC_PERCENTAGE and not oq.is_deleted:
                     if oq.is_suboptions:
+                        print(oq.option_name)
                         if request.data.get('options').get(oq.option_name):
                             basic_percentage_temp_arr.append(request.data.get('options').get(oq.option_name, 1)[1])
                     else:
@@ -87,6 +88,7 @@ class CalculatePriceViewSet(APIView):
                     else:
                         total_price = total_price + quantity * request.data.get('options').get(oq.option_name, 0)
             print(total_price)
+            print(basic_percentage_temp_arr)
             for i in basic_percentage_temp_arr:
                 total_price = total_price + basic_price * (i / 100)    
             total_price = total_price + product.setup_cost
