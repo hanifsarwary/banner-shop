c3716df8bd8ce1a0ac3278ea8baa46b2622ee471
hanifsarwary <l154368@lhr.nu.edu.pk>
2020-04-07 20:50:27 +0500

    Bug fixed

--- backend/bannershop/api/views/pricing.py
+++ backend/bannershop/api/views/pricing.py
@@ -35,16 +35,17 @@ class CalculatePriceViewSet(APIView):
 
             elif product.price_type == PRODUCT_TWO_OPTION:
                 option_names = product.price_details.get('sequence')
-
+                quantity = int(request.data['options'].pop('Quantity'))
                 basic_price = TwoDependentSubOption.objects.filter(first_sub_option__option__option_name=option_names[0]).filter(
                     second_sub_option__option__option_name=option_names[1]).filter(
                         first_sub_option__name=request.data['options'].pop(option_names[0])[0]).filter(
                             second_sub_option__name=request.data['options'].pop(option_names[1])[0]
                         ).first().price
+                
             elif product.price_type == PRODUCT_THREE_OPTION:
 
                 option_names = product.price_details.get('sequence')
-
+                quantity = int(request.data['options'].pop('Quantity'))
                 basic_price = ThreeDependentSubOption.objects.filter(first_sub_option__option__option_name=option_names[0]).filter(
                     second_sub_option__option__option_name=option_names[1]).filter(
                     third_sub_option__option__option_name=option_names[2]).filter(
