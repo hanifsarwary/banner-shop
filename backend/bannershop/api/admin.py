94fa419ba37a0d97ed01ed84f4743af49cccff45
hanifsarwary <l154368@lhr.nu.edu.pk>
2020-04-19 13:04:11 +0500

    Order added in admin

--- backend/bannershop/api/admin.py
+++ backend/bannershop/api/admin.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from api.models import (
     Product, ProductOrder, Category, Option, SubOption, Customer, Coupon, CustomQuote, ContactRequest, ProductOrderOption,
-    TwoDependentSubOption, ThreeDependentSubOption
+    TwoDependentSubOption, ThreeDependentSubOption, Order
     )
 
 
@@ -12,6 +12,7 @@ admin.site.register(Product)
 admin.site.register(ProductOrder)
 admin.site.register(Category)
 admin.site.register(Option)
+admin.site.register(order)
 admin.site.register(SubOption)
 admin.site.register(Customer)
 admin.site.register(Coupon)
