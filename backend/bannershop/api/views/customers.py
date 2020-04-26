1e33646a1ef959a88b3aeeebeacfe44c492889cd
hanifsarwary <l154368@lhr.nu.edu.pk>
2020-04-26 14:31:21 +0500

    company names api created

--- backend/bannershop/api/views/customers.py
+++ backend/bannershop/api/views/customers.py
@@ -2,12 +2,19 @@
 from __future__ import unicode_literals
 
 from rest_framework.generics import ListCreateAPIView
-
+from rest_framework.views import APIView
 from api.models import Customer
 from api.serializers.customers import CustomerSerializer
-
+from rest_framework.response import Response
 
 class CustomerListViewSet(ListCreateAPIView):
+    
     serializer_class = CustomerSerializer
     queryset = Customer.objects.all().order_by('-id')
     
+
+class CompanyNamesListView(APIView):
+
+    def get(self, request):
+
+        return Response({'names': Customer.obejcts.all().values('company_name')})
