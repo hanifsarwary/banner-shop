9ffa185548005b294f934ff48c7d813b22e2b659
hanifsarwary <l154368@lhr.nu.edu.pk>
2020-04-27 23:45:49 +0500

    bug fixed in customer create

--- backend/bannershop/api/serializers/customers.py
+++ backend/bannershop/api/serializers/customers.py
@@ -1,7 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from api.serializers.users import CreateUserSerializer
 from api.models import Customer
-
+from django.contrib.auth.models import User
 
 class CustomerSerializer(ModelSerializer):
 
@@ -9,3 +9,10 @@ class CustomerSerializer(ModelSerializer):
     class Meta:
         model = Customer
         fields = '__all__'
+    
+    def create(self, validated_data):
+        user = validated_data.pop('user')
+        user = User.objects.create(user)
+        return Customer.objects.create(user=user, **validated_data)
+
+        
