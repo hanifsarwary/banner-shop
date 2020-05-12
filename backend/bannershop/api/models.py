7decab1c2b0c9634159a47f32e33c0ac03b58670
hanifsarwary <l154368@lhr.nu.edu.pk>
2020-05-12 22:06:49 +0500

    new app created

--- backend/bannershop/api/models.py
+++ backend/bannershop/api/models.py
@@ -282,49 +282,6 @@ class ProofHistory(models.Model):
     proof_status = models.CharField(max_length=256, null=True, choices=CustomOrder.PROOF_STATUS_CHOICES)
 
 
-class Order(models.Model):
-
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    customer_required_date = models.DateField(null=True, blank=True)
-    details = models.TextField(max_length=512)
-    order_number = models.UUIDField(default=uuid.uuid4, editable=False)
-    start_date = models.DateField(null=True, blank=True)
-    status = models.CharField(choices=CustomOrder.STATUS_CHOICES, max_length=64)
-    special_instruction = models.TextField(null=True)
-
-    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
-
-    def __str__(self):
-        return str(self.order_number)
-
-
-
-class ProductOrder(models.Model):
-    product = models.ForeignKey(Product, related_name='product_productorders', on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, related_name='order_productorders' ,on_delete=models.CASCADE)
-
-    custom_image = models.FileField(null=True, blank=True, upload_to='images/order_custom/')
-    special_note = models.CharField(max_length=128)
-    total_price = models.FloatField(default=0)
-    total_weight = models.FloatField(default=0)
-    product_units = models.FloatField(default=1)
-
-    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
-
-    def __str__(self):
-        return self.product.__str__() + ' ------- ' + self.order.__str__()
-
-class ProductOrderOption(models.Model):
-
-    product_order = models.ForeignKey(ProductOrder,related_name='product_order_options', on_delete=models.CASCADE)
-    option = models.ForeignKey(Option, on_delete=models.CASCADE)
-    sub_option = models.ForeignKey(SubOption, on_delete=models.CASCADE, null=True, blank=True)
-    quantity = models.IntegerField(default=0, null=True, blank=True)
-    price = models.FloatField(null=True, blank=True)
-
-
 class CustomQuote(models.Model):
     additional_requirements = models.CharField(max_length=512)
     company_name = models.CharField(max_length=64)
