5e17ad0d332309f23b9521161a03b0c4bad83dc5
hanifsarwary <l154368@lhr.nu.edu.pk>
2020-06-03 20:38:55 +0500

    search filters added

--- backend/bannershop/api/views/customers.py
+++ backend/bannershop/api/views/customers.py
@@ -7,13 +7,54 @@ from api.models import Customer
 from api.serializers.customers import CustomerSerializer, CustomerStatusUpdateSerializer
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
+from api.views.custom_orders import CustomOrderListViewSet
 
 class CustomerListViewSet(ListCreateAPIView):
     
     serializer_class = CustomerSerializer
-    queryset = Customer.objects.all().order_by('-id')
-    filter_backends = [DjangoFilterBackend, ]
-    filterset_fields = ['status']
+    queryset = Customer.objects.all()
+  
+    def filter_company_name(self, queryset, company_name):
+        if company_name:
+            queryset = queryset.filter(company_name__icontains=company_name)
+        return queryset
+    
+    def filter_first_name(self, queryset, first_name):
+        if first_name:
+            queryset = queryset.filter(user__first_name__icontains=first_name)
+        return queryset
+    
+    def filter_last_name(self, queryset, last_name):
+        if last_name:
+            queryset = queryset.filter(user__last_name__icontains=last_name)
+        return queryset
+    
+    def filter_username(self, queryset, username):
+        if username:
+            queryset = queryset.filter(user__username__icontains=username)
+        return queryset
+    
+    def filter_email(self, queryset, email):
+        if email:
+            queryset = queryset.filter(user__email__icontains=email)
+        return queryset
+    
+    def filter_city(self, queryset, city):
+        if city:
+            queryset = queryset.filter(city__icontains=city)
+        return queryset
+
+    def post(self, request):
+        queryset = CustomOrderListViewSet().filter_status(self.get_queryset(), request.data.get('status'))
+        queryset = self.filter_company_name(queryset, self.request.data.get('company_name_search'))
+        queryset = self.filter_first_name(queryset, self.request.data.get('first_name_search'))
+        queryset = self.filter_last_name(queryset, self.request.data.get('last_name_search'))
+        queryset = self.filter_username(queryset, self.request.data.get('username_search'))
+        queryset = self.filter_email(queryset, self.request.data.get('email_search'))
+        queryset = self.filter_company_name(queryset, self.request.data.get('city_search'))
+        return Response({
+            'results': self.serializer_class(queryset).data
+        })
     
 
 class CompanyNamesListView(APIView):
